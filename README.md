ec9ce975509e6e852f7a512466bed55277bd05c2
Nate Schmidt <nschmidt1@hotmail.com>
2017-04-24 15:37:53 -0700

    Full update everything updated

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 #Aloha Project
 
-This is a website about Hawaiian stuff or stuff.
+##Goals
+*Create a 1 page responsive website utilizing HTML, CSS, and JavaScript.
+*Build with mobile first design and optimize for moblie tablet and desktop.
 
-##Things I might know
+##Things I Used
+*Git Bash (https://git-for-windows.github.io/)
+*Google Chrome (https://www.google.com/chrome/)
+*Google Chrome Developer Tools
+*HTML Validator (https://validator.w3.org/nu/#file)
+*CSS Validator (https://jigsaw.w3.org/css-validator/#validate_by_upload)
+*JavaScript
+*JQuery (https://jquery.com/)
+*Visual Studio Code (https://code.visualstudio.com/)
+*Flickity (http://flickity.metafizzy.co/)
+*Pesticide(Chrome Extension)
 
-- thing1
-- thing2
-- thing3
+##Things I Learned
+*Do a lot of tests inside Chrome developer tools before changing values inside of css   document.
+
+*Thouroghly plan out entire html tree after having looked at all different levels of optimization.
+
+*I now have a basic understanding and some experience with flexbox.
--- js/main.js
+++ js/main.js
@@ -1,1 +1,55 @@
 console.log('Hello World.');
+$(function(){
+  $(".carasell").flickity({
+  prevNextButtons: false, 
+  cellAlign: 'left',
+  wrapAround: true,
+  autoPlay: true,
+
+})
+
+$(".carasell").children().children("div").siblings().css('text-transform: upercase')
+  
+
+
+//TAKE FROM: https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
+  $(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+$('button').on('click', function(){
+
+  var input=$('input').val(); 
+
+  if(input == ""){
+    alert('Please enter a valid email!')
+  }
+  else{
+    alert('Thanks for signing up!')
+  }
+
+})
+
+
+})
